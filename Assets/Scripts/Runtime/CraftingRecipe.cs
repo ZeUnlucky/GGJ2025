c3207322550f9db9d1ab9df02fc32482fa224e74
GDIVX <yarin107@gmail.com>
2025-01-24 10:22:27 +0200

    created crafting system

---
+++ Assets/Scripts/Runtime/CraftingRecipe.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Runtime
+{
+    [CreateAssetMenu(fileName = "Crafting Recipe", menuName = "Game/Crafting/Recipe", order = 0)]
+    public class CraftingRecipe : ScriptableObject
+    {
+        [SerializeField] private string _inputTagA, _inputTagB;
+        [SerializeField] private GameObject _output;
+
+        public string InputA => _inputTagA;
+        public string InputB => _inputTagB;
+        public GameObject Output => _output;
+    }
+}
